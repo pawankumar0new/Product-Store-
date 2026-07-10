ede133295f59b2dce777e4df972399516869fad6
Pawan kumar <pawankumarfromhuc@gmail.com>
2026-07-10 10:27:09 +0500

    updated code

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,6 +6,9 @@ const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    FRONTEND_URL: process.env.FRONTEND_URL,
 }
 
 export default ENV;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import ENV from './config/env'
 import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors'
 
 
 const app = express()
 
+app.use(cors({origin: ENV.FRONTEND_URL}))
 app.use(clerkMiddleware())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true})) // For parsing the form data (like html)
 app.get("/", (req,res)=>{
     res.json({success:true})
 })
